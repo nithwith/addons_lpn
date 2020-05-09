97c9a4fdcc5da0b39b8b2729ba9f4809b4ff3003
nithwith <nithwith@live.fr>
2020-05-09 16:16:28 +0200

    [ADD] Invoice report "Coup de pouce velo"

---
+++ invoice_report_coup_pouce_velo/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2019 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Invoice  report coup pouce velo',
+    'version': '12.0.1.0.0',
+    'author': 'Théo MARTY',
+    'license': 'AGPL-3',
+    'category': 'Accounting',
+    'website': 'https://github.com/nithwith',
+    'depends': [
+        'account',
+        ],
+    'data': [
+        'data/report.xml',
+        'report/report_invoice_coup_pouce_velo.xml',
+        ],
+    'installable': True,
+    'auto_install': False,
+}
